c77813ab530c03144f4a539a8cff6a10dbd166e8
ishbosamiya <ishbosamiya@gmail.com>
2021-01-17 13:34:43 +0530

    render normals of sphere

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -39,18 +39,24 @@ fn main() {
     ppm.write_to_file("image.ppm").unwrap();
 }
 
-fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin() - center;
     let a = ray.direction().dot(&ray.direction());
     let b = 2.0 * oc.dot(&ray.direction());
     let c = oc.dot(&oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    return discriminant > 0.0;
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    return (-b - discriminant.sqrt()) / (2.0 * a);
 }
 
 fn ray_color(ray: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let sphere_center = Vec3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(&sphere_center, 0.5, ray);
+    if t > 0.0 {
+        let normal = (ray.at(t) - &sphere_center).unit_vector();
+        return 0.5 * (normal + Vec3::new(1.0, 1.0, 1.0));
     }
     let unit_direction = ray.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
